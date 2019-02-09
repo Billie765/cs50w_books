ea2dcdcb12d7341e07470543bcac3fb2dfd8fdde
Igor Starchenko <igor.starchenko@gmail.com>
2019-02-09 17:52:57 +0200

    Updated login page, added logout function, upgraded book page

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, session, render_template, request, jsonify
+from flask import Flask, session, render_template, request, jsonify, url_for, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,45 +20,62 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
+def home():
+    #session['user'] = '1'
+    return redirect(url_for("index"))
+
+@app.route("/index")
 def index():
-    return render_template('home.html')
+    return render_template('home.html', username=session.get('user'))
+
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "POST":
         login = request.form.get("login")
         password = request.form.get("password")
-        if db.execute("SELECT * from users WHERE login = :login",{"login":login}).rowcount == 0:
+        if db.execute("SELECT * from users WHERE login = :login",
+        {"login":login}).rowcount == 0:
             return render_template("error.html", message="No such user")
             #return render_template("end.html", login = login, password=password)
         else:
-            result = db.execute("SELECT password from users where login = :login", {'login':login}).fetchone()[0]
-            if password == result:
+            result = db.execute("SELECT * from users where login = :login",
+            {'login':login}).fetchone()
+            if password == result.password:
+                session['user'] = result.login
+                session['id'] = result.id
                 return render_template("success.html", message="You logged in")
             else:
-                return render_template("error.html", message=f"login: {login}. password: {password}. result: {result}")
+                return render_template("error.html",
+                message="Login/password combination you typed is wrong)
     else:
         return render_template("login.html")
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         login = request.form.get("login")
         password = request.form.get("password")
-        if db.execute("SELECT * from users where login=:login", {'login':login}).rowcount == 0:
-            db.execute("INSERT into users (login, password) VALUES (:login, :password)", {'login':login, 'password':password})
+        if db.execute("SELECT * from users where login=:login",
+         {'login':login}).rowcount == 0:
+            db.execute("INSERT into users (login, password) \
+            VALUES (:login, :password)", {'login':login, 'password':password})
             db.commit()
             return render_template("success.html", message="Congrats")
         else:
-            return render_template("error.html", message="User with this login already exists")
+            return render_template("error.html",
+            message="User with this login already exists")
     else:
         return render_template("register.html")
 
 @app.route("/api/<string:isbn>", methods=["GET"])
 def return_data(isbn):
-    book = db.execute("SELECT * from books where isbn=:isbn", {'isbn':isbn}).fetchone()
+    book = db.execute("SELECT * from books where isbn=:isbn",
+    {'isbn':isbn}).fetchone()
     if book is None:
         return "Sorry, book not found", 404
     else:
-        d = {'isbn': book.isbn, 'author': book.author, 'title': book.title, 'year': book.year}
+        d = {'isbn': book.isbn, 'author': book.author, 'title': book.title,
+        'year': book.year}
         return jsonify(d)
 
 @app.route("/search", methods=["GET","POST"])
@@ -69,11 +86,22 @@ def search():
         isbn = request.form.get("isbn")
         author = request.form.get("author")
         title = request.form.get("title")
-        querry = f"SELECT * from books where isbn like '%{isbn}%' and author like '%{author}%' and title like '%{title}%'"
+        querry = f"SELECT * from books where isbn like '%{isbn}%' \
+        and author like '%{author}%' and title like '%{title}%'"
         results = db.execute(querry).fetchall()
-        return render_template("result.html", isbn=isbn, author=author, title=title, results=results)
+        return render_template("result.html", isbn=isbn, author=author,
+        title=title, results=results)
 
 @app.route("/book/<string:isbn>")
 def book(isbn):
-    book = db.execute("SELECT * FROM books where isbn=:isbn", {'isbn':isbn}).fetchone()
-    return render_template('book.html', title=book.title, author=book.author, isbn=book.isbn, year=book.year)
+    book = db.execute("SELECT * FROM books where isbn=:isbn",
+    {'isbn':isbn}).fetchone()
+    return render_template('book.html', title=book.title, author=book.author,
+    isbn=book.isbn, year=book.year, username=session.get('user'),
+    user_id=session.get('id'))
+
+@app.route('/logout')
+def logout():
+    session['user'] = None
+    session['id'] = None
+    return redirect(url_for('index'))
